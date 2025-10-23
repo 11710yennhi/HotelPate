6e08b19b77c33ad27fa71f0b34ddfa8e1595330f
LE QUOC BAO <lequocbao29625@gmail.com>
2025-10-23 19:02:12 +0700

    add entity ChiTietChiPhiPhatSinh

---
+++ src/entity/ChiTietChiPhiPhatSinh.java
@@ -0,0 +1,79 @@
+package entity;
+
+import java.util.Objects;
+
+public class ChiTietChiPhiPhatSinh {
+    private PhieuDatPhong phieuDatPhong;
+    private ChiPhiPhatSinh chiPhiPhatSinh;
+    private int soLuong;
+    private double thanhTien; // thuộc tính dẫn xuất
+
+    public ChiTietChiPhiPhatSinh() {
+    }
+
+    public ChiTietChiPhiPhatSinh(PhieuDatPhong phieuDatPhong, ChiPhiPhatSinh chiPhiPhatSinh, int soLuong) {
+        this.phieuDatPhong = phieuDatPhong;
+        this.chiPhiPhatSinh = chiPhiPhatSinh;
+        this.soLuong = soLuong;
+        tinhThanhTien();
+    }
+
+    public PhieuDatPhong getPhieuDatPhong() {
+        return phieuDatPhong;
+    }
+
+    public void setPhieuDatPhong(PhieuDatPhong phieuDatPhong) {
+        this.phieuDatPhong = phieuDatPhong;
+    }
+
+    public ChiPhiPhatSinh getChiPhiPhatSinh() {
+        return chiPhiPhatSinh;
+    }
+
+    public void setChiPhiPhatSinh(ChiPhiPhatSinh chiPhiPhatSinh) {
+        this.chiPhiPhatSinh = chiPhiPhatSinh;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+        tinhThanhTien();
+    }
+
+    public double getThanhTien() {
+        return thanhTien;
+    }
+
+    public void tinhThanhTien() {
+        if (chiPhiPhatSinh != null)
+            this.thanhTien = chiPhiPhatSinh.getGia() * soLuong;
+        else
+            this.thanhTien = 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(phieuDatPhong, chiPhiPhatSinh);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        ChiTietChiPhiPhatSinh other = (ChiTietChiPhiPhatSinh) obj;
+        return Objects.equals(phieuDatPhong, other.phieuDatPhong)
+                && Objects.equals(chiPhiPhatSinh, other.chiPhiPhatSinh);
+    }
+
+    @Override
+    public String toString() {
+        return "ChiTietChiPhiPhatSinh [phieuDatPhong=" + phieuDatPhong.getMaPhieuDatPhong()
+                + ", chiPhiPhatSinh=" + chiPhiPhatSinh.getTenChiPhiPhatSinh()
+                + ", soLuong=" + soLuong + ", thanhTien=" + thanhTien + "]";
+    }
+}
