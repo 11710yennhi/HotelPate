4c31ac574664ae0963fc1e35743a5dd0e35ddbc5
11710yennhi <1710yennhi@gmail.com>
2025-10-23 21:14:19 +0700

    Cập nhật lớp phiếu đặt phòng

--- src/dao/PhieuDatPhong_DAO.java
+++ src/dao/PhieuDatPhong_DAO.java
@@ -2,14 +2,18 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Date;
+
 import connectDB.ConnectDB;
+import entity.KhachHang;
+import entity.NhanVien;
 import entity.PhieuDatPhong;
 
 public class PhieuDatPhong_DAO {
 
-    // Thêm phiếu đặt phòng mới vào CSDL
+
     public boolean themPhieuDatPhong(PhieuDatPhong pdt) {
         String query = "INSERT INTO PhieuDatPhong (maPhieuDatPhong, maKhachHang, maNhanVien, ngayTao, trangThai) "
                      + "VALUES (?, ?, ?, ?, ?)";
@@ -30,5 +34,50 @@ public class PhieuDatPhong_DAO {
             e.printStackTrace();
         }
         return false;
+    }
+
+ 
+    public boolean xoaPhieuDatPhong(String maPhieuDatPhong) {
+        String query = "DELETE FROM PhieuDatPhong WHERE maPhieuDatPhong = ?";
+
+        try (Connection con = ConnectDB.getConnection();
+             PreparedStatement ps = con.prepareStatement(query)) {
+
+            ps.setString(1, maPhieuDatPhong);
+
+            int rowsAffected = ps.executeUpdate();
+            return rowsAffected > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    public PhieuDatPhong timPhieuDatPhongTheoMa(String maPhieuDatPhong) {
+        String query = "SELECT * FROM PhieuDatPhong WHERE maPhieuDatPhong = ?";
+        PhieuDatPhong pdt = null;
+
+        try (Connection con = ConnectDB.getConnection();
+             PreparedStatement ps = con.prepareStatement(query)) {
+
+            ps.setString(1, maPhieuDatPhong);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                pdt = new PhieuDatPhong(
+                    rs.getString("maPhieuDatPhong"),
+                    new KhachHang(rs.getString("maKhachHang")),
+                    new NhanVien(rs.getString("maNhanVien")),
+                    rs.getDate("ngayTao").toLocalDate(),
+                    rs.getString("trangThai")
+                );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return pdt;
     }
 }
--- src/entity/PhieuDatPhong.java
+++ src/entity/PhieuDatPhong.java
@@ -6,125 +6,176 @@ import java.util.List;
 import java.util.Objects;
 
 public class PhieuDatPhong {
-   private String maPhieuDatPhong;
-   private KhachHang khachHang;
-   private NhanVien nhanVien;
-   private LocalDate ngayTao;
-   private String trangThai;
-   private double tongTienPhong; // thuộc tính dẫn xuất
-   private double tienCoc; // thuộc tính dẫn xuất
-   private List<ChiTietPhieuDatPhong> dsChiTiet;
-	public PhieuDatPhong() {
-		
+    private String maPhieuDatPhong;
+    private KhachHang khachHang;
+    private NhanVien nhanVien;
+    private LocalDate ngayTao;
+    private String trangThai;
+
+    private double tongTienPhong;             // thuộc tính dẫn xuất
+    private double tongTienChiPhiPhatSinh;    // thuộc tính dẫn xuất
+    private double tongTien;                  // thuộc tính dẫn xuất
+    private double tienCoc;                   // thuộc tính dẫn xuất
+
+    private List<ChiTietPhieuDatPhong> dsChiTiet;
+    private List<ChiTietChiPhiPhatSinh> dsChiPhiPhatSinh;
+
+    public PhieuDatPhong() {
+    }
+
+    public PhieuDatPhong(String maPhieuDatPhong) {
+        this.maPhieuDatPhong = maPhieuDatPhong;
+    }
+
+    public PhieuDatPhong(String maPhieuDatPhong, KhachHang khachHang, NhanVien nhanVien,
+                         LocalDate ngayTao, String trangThai) {
+        this.maPhieuDatPhong = maPhieuDatPhong;
+        this.khachHang = khachHang;
+        this.nhanVien = nhanVien;
+        this.ngayTao = ngayTao;
+        this.trangThai = trangThai;
+        this.dsChiTiet = new ArrayList<>();
+        this.dsChiPhiPhatSinh = new ArrayList<>();
+        capNhatTongTien();
+    }
+
+    // Getter & Setter
+    public String getMaPhieuDatPhong() {
+        return maPhieuDatPhong;
+    }
+
+    public void setMaPhieuDatPhong(String maPhieuDatPhong) {
+        this.maPhieuDatPhong = maPhieuDatPhong;
+    }
+
+    public KhachHang getKhachHang() {
+        return khachHang;
+    }
+
+    public void setKhachHang(KhachHang khachHang) {
+        this.khachHang = khachHang;
+    }
+
+    public NhanVien getNhanVien() {
+        return nhanVien;
+    }
+
+    public void setNhanVien(NhanVien nhanVien) {
+        this.nhanVien = nhanVien;
+    }
+
+    public LocalDate getNgayTao() {
+        return ngayTao;
+    }
+
+    public void setNgayTao(LocalDate ngayTao) {
+        this.ngayTao = ngayTao;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(String trangThai) {
+        this.trangThai = trangThai;
+    }
+
+    public List<ChiTietPhieuDatPhong> getDsChiTiet() {
+        return dsChiTiet;
+    }
+
+    public void setDsChiTiet(List<ChiTietPhieuDatPhong> dsChiTiet) {
+        this.dsChiTiet = dsChiTiet;
+        capNhatTongTien();
+    }
+
+    public List<ChiTietChiPhiPhatSinh> getDsChiPhiPhatSinh() {
+        return dsChiPhiPhatSinh;
+    }
+
+    public void setDsChiPhiPhatSinh(List<ChiTietChiPhiPhatSinh> dsChiPhiPhatSinh) {
+        this.dsChiPhiPhatSinh = dsChiPhiPhatSinh;
+        capNhatTongTien();
+    }
+
+    // -------------------------------
+    // Các phương thức xử lý chính
+    // -------------------------------
+
+    public void themChiTiet(ChiTietPhieuDatPhong ct) {
+        dsChiTiet.add(ct);
+        capNhatTongTien();
+    }
+
+    public void themChiPhiPhatSinh(ChiTietChiPhiPhatSinh cp) {
+        dsChiPhiPhatSinh.add(cp);
+        capNhatTongTien();
+    }
+
+    // ✅ Cập nhật riêng từng phần và tổng cộng
+    public void capNhatTongTien() {
+        tongTienPhong = 0;
+        tongTienChiPhiPhatSinh = 0;
+
+        // Tính tổng tiền phòng
+        if (dsChiTiet != null) {
+            for (ChiTietPhieuDatPhong ct : dsChiTiet) {
+                tongTienPhong += ct.getPhong().getLoaiPhong().getGia();
+            }
+        }
+
+        // Tính tổng chi phí phát sinh
+        if (dsChiPhiPhatSinh != null) {
+            for (ChiTietChiPhiPhatSinh cp : dsChiPhiPhatSinh) {
+                tongTienChiPhiPhatSinh += cp.getThanhTien();
+            }
+        }
+
+        // Tổng cộng và tiền cọc
+        tongTien = tongTienPhong + tongTienChiPhiPhatSinh;
+        tienCoc = tongTienPhong * 0.5; // chỉ tính 50% tiền phòng
+    }
+
+    public double getTongTienPhong() {
+        return tongTienPhong;
+    }
+
+    public double getTongTienChiPhiPhatSinh() {
+        return tongTienChiPhiPhatSinh;
+    }
+
+    public double getTongTien() {
+        return tongTien;
+    }
+
+    public double getTienCoc() {
+        return tienCoc;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(maPhieuDatPhong);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        PhieuDatPhong other = (PhieuDatPhong) obj;
+        return Objects.equals(maPhieuDatPhong, other.maPhieuDatPhong);
+    }
+
+	@Override
+	public String toString() {
+		return "PhieuDatPhong [maPhieuDatPhong=" + maPhieuDatPhong + ", khachHang=" + khachHang + ", nhanVien="
+				+ nhanVien + ", ngayTao=" + ngayTao + ", trangThai=" + trangThai + ", tongTienPhong=" + tongTienPhong
+				+ ", tongTienChiPhiPhatSinh=" + tongTienChiPhiPhatSinh + ", tongTien=" + tongTien + ", tienCoc="
+				+ tienCoc + ", dsChiTiet=" + dsChiTiet + ", dsChiPhiPhatSinh=" + dsChiPhiPhatSinh + "]";
 	}
-	
-	public PhieuDatPhong(String maPhieuDatPhong) {
-		this.maPhieuDatPhong = maPhieuDatPhong;
-	}
 
-	public PhieuDatPhong(String maPhieuDatPhong, KhachHang khachHang, NhanVien nhanVien, LocalDate ngayTao,
-			String trangThai) {
-		this.maPhieuDatPhong = maPhieuDatPhong;
-		this.khachHang = khachHang;
-		this.nhanVien = nhanVien;
-		this.ngayTao = ngayTao;
-		this.trangThai = trangThai;
-		this.dsChiTiet = new ArrayList<ChiTietPhieuDatPhong>();
-		capNhatTongTien();
-	}
-	
-	
-	 public String getMaPhieuDatPhong() {
-		return maPhieuDatPhong;
-	}
-
-	public void setMaPhieuDatPhong(String maPhieuDatPhong) {
-		this.maPhieuDatPhong = maPhieuDatPhong;
-	}
-
-	public KhachHang getKhachHang() {
-		return khachHang;
-	}
-
-	public void setKhachHang(KhachHang khachHang) {
-		this.khachHang = khachHang;
-	}
-
-	public NhanVien getNhanVien() {
-		return nhanVien;
-	}
-
-	public void setNhanVien(NhanVien nhanVien) {
-		this.nhanVien = nhanVien;
-	}
-
-	public LocalDate getNgayTao() {
-		return ngayTao;
-	}
-
-	public void setNgayTao(LocalDate ngayTao) {
-		this.ngayTao = ngayTao;
-	}
-
-	public String getTrangThai() {
-		return trangThai;
-	}
-
-	public void setTrangThai(String trangThai) {
-		this.trangThai = trangThai;
-	}
-
-	public List<ChiTietPhieuDatPhong> getDsChiTiet() {
-		return dsChiTiet;
-	}
-
-	public void setDsChiTiet(List<ChiTietPhieuDatPhong> dsChiTiet) {
-		this.dsChiTiet = dsChiTiet;
-	}
-
-	public void themChiTiet(ChiTietPhieuDatPhong ct) {
-	        dsChiTiet.add(ct);
-	        capNhatTongTien();
-	    }
-	 public void capNhatTongTien() {
-	        tongTienPhong = 0;
-	        for (ChiTietPhieuDatPhong ct : dsChiTiet) {
-	            tongTienPhong += ct.getPhong().getLoaiPhong().getGia();
-	        }
-	        tienCoc = tongTienPhong * (0.5);
-	    }
-
-	    public double getTongTienPhong() {
-	        return tongTienPhong;
-	    }
-
-	    public double getTienCoc() {
-	        return tienCoc;
-	    }
-
-		@Override
-		public int hashCode() {
-			return Objects.hash(maPhieuDatPhong);
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			PhieuDatPhong other = (PhieuDatPhong) obj;
-			return Objects.equals(maPhieuDatPhong, other.maPhieuDatPhong);
-		}
-
-		@Override
-		public String toString() {
-			return "PhieuDatPhong [maPhieuDatPhong=" + maPhieuDatPhong + ", khachHang=" + khachHang + ", nhanVien="
-					+ nhanVien + ", ngayTao=" + ngayTao + ", trangThai=" + trangThai + ", tongTienPhong="
-					+ tongTienPhong + ", tienCoc=" + tienCoc + ", dsChiTiet=" + dsChiTiet + "]";
-		}
-	
    
 }
